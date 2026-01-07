ff1cf42f6e1452b3acdaf40bcc7a783a68d72104
Marley <warburtonmarley@proton.me>
2026-01-07 14:19:49 +0000

    Delete hyprdeck directory

--- hyprdeck/nvim/lua/config/nvim-treesitter-config.lua
+++
@@ -1,9 +0,0 @@
--- ~/.config/nvim/lua/config/nvim-treesitter-config.lua
-require("nvim-treesitter.configs").setup({
-    ensure_installed = "maintained",  -- Automatically install maintained parsers
-    highlight = {
-        enable = true,  -- Enable Treesitter-based syntax highlighting
-        additional_vim_regex_highlighting = false,
-    },
-})
-
